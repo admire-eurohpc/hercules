1ceaf3b4d2f916813a1b4d39d70e843eed0bc5e6
MASerruya <mariandr@inf.uc3m.es>
2020-01-22 13:20:06 +0100

    LOCAL policy implemented

--- inc/policies.h
+++ inc/policies.h
@@ -6,9 +6,10 @@
 #define HASHED_			2
 #define CRC16_			3
 #define CRC64_			4
+#define LOCAL_ 			5
 
 //Method specifying the policy.
-int32_t set_policy (const char * policy, int32_t blocks);
+int32_t set_policy (const char * policy, int32_t blocks, int32_t matching_node_conn);
 
 //Method retrieving the server that will receive the following message attending a policy.
 int32_t find_server (int32_t n_servers, uint64_t n_msg, const char * fname);
