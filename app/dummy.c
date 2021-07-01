0af41cd16904f1768838041de846a69a9da442d3
MASerruya <mariandr@inf.uc3m.es>
2021-07-01 15:33:08 +0200

    Pablos use case completed available in app/dummy.c

--- app/dummy.c
+++ app/dummy.c
@@ -4,7 +4,7 @@
 #include <string.h>
 #include "hercules.h"
 
-#define NUM_DATASETS	10000
+#define NUM_DATASETS	1
 
 int32_t main (int32_t argc, char **argv) 
 {
@@ -17,13 +17,13 @@ int32_t main (int32_t argc, char **argv)
 	char hostfile[]	 = "./hostfile";
 
 	//Hercules init -- Attached deploy
-	if (hercules_init(rank, 2097152, 5555, 5569, 1048576, metadata) == -1) exit(-1);
+	if (hercules_init(rank, 2048, 5555, 5569, 1024, metadata) == -1) exit(-1);
 
 	//Metadata server
 	if (stat_init(localhost, 5569, rank) == -1) exit(-1);
 
 	//Imss deploy
-	if (init_imss("imss://berries", hostfile, 1, 5555, 1048576, ATTACHED, NULL) == -1) exit(-1);
+	if (init_imss("imss://berries", hostfile, 1, 5555, 1024, ATTACHED, NULL) == -1) exit(-1);
 
 	//Dump data -- Remark: DATA MUST BE IN DYNAMIC MEMORY
 	for(int i = 0; i < NUM_DATASETS; ++i){
@@ -32,17 +32,19 @@ int32_t main (int32_t argc, char **argv)
 		char dataset_uri[32];
 		sprintf(dataset_uri, "imss://berries/%d", i);
 		//Create dataset, 1 Block of 1 Kbyte 
-		if ((datasetd_ = create_dataset(dataset_uri, "RR", 1, 1, NONE)) < 0) exit(-1);
+		if ((datasetd_ = create_dataset(dataset_uri, "RR", 1, 1024, NONE)) < 0) exit(-1);
 
-		char * buffer = (char *) malloc(1024*sizeof(char));
+		char * buffer = (char *) malloc(1024 * 1024 * sizeof(char));
 		//Fill the buffer with \n
-		memset((void*) buffer, '0', 1024);
+		memset((void*) buffer, 0, 1024 * 1024);
 		//Copy the used data
 		char const * testdata = "\na\nab\nabc\nabcd\nabcde\nabcdef\nabcdefg\na\nbb\nccc\ndddd\neeeee\nffffff\nggggggg\n1\n22\n\n333\n";
 		memcpy(buffer, testdata, strlen(testdata));
 
 		//Set the data in 2 Blocks
 		int32_t data_sent = set_data(datasetd_, 0, (unsigned char*)buffer);
+
+		printf("AFTR SET\n");
 
 		release_dataset(datasetd_);	
 	}
@@ -55,11 +57,11 @@ int32_t main (int32_t argc, char **argv)
 
 		datasetd_ = open_dataset(dataset_uri);
 
-		char * buffer = (char *) malloc(1024*sizeof(char));
+		char * buffer = (char *) malloc(1024 * 1024 * sizeof(char));
 
 		get_data(datasetd_, 0, (unsigned char*)buffer);
 
-		//printf("DATA %s: %s\n", dataset_uri, buffer);
+		printf("DATA %s: %s\n", dataset_uri, buffer);
 		free(buffer);
 		release_dataset(datasetd_);	
 	}
@@ -82,6 +84,7 @@ int32_t main (int32_t argc, char **argv)
 //
 //	free(buffer);
 //	free(it);
+
 	stat_release();
 	hercules_release(0);
 
