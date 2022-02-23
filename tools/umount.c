055e9f5b17f6fa02dbed18076a45804d1b3e79f6
Javier Garcia Blas <fjblas@inf.uc3m.es>
2022-02-23 06:10:52 +0100

    Added preloaded version initial steps

---
+++ tools/umount.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <syslog.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <inttypes.h>
+#include <stdlib.h>
+#include <time.h>
+#include <limits.h>
+
+
+int main () {
+   char buff[16];
+   int pid;
+
+   int run = open("/var/run/imss.pid", O_RDONLY);
+   read(run, buff, 16);
+
+   sscanf(buff, "%u" , &pid);
+
+   close(run);
+   unlink("/var/run/imss.pid");
+
+   kill(pid, SIGKILL);
+
+
+   return 1;
+}
