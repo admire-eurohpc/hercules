b682b9d67d6661c2f19aacb5a17729cb21802f3f
Cosmin Petre <cpetre@pa.uc3m.es>
2022-09-20 14:20:54 +0200

    fixed compilation issue

--- src/arg_parser.c
+++ src/arg_parser.c
@@ -48,7 +48,7 @@ static error_t parse_opt (int key, char * arg, struct argp_state * state)
 {
     /* Get the input argument from argp_parse, which we
         know is a pointer to our arguments structure */
-    struct arguments *args = state->input;
+    struct arguments *args = (struct arguments *) state->input;
 
     switch (key)
         {
